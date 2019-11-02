0dab36983e613649c4b3000fc483b559cfff46fd
Sameer Puri <sameer@users.noreply.github.com>
2019-11-02 14:21:44 -0500

    Add reddit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { r } from "~reddit";
+
 // https://stackoverflow.com/questions/1484506/random-color-generator
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -11,3 +13,6 @@ function getRandomColor() {
 setInterval(() => {
     document.getElementsByTagName('a-box')[0].setAttribute('color', getRandomColor());
 }, 1000);
+
+
+r.then(r => r.getHot().then(list => list.map(post => post.title)).then(console.log));
--- src/reddit.ts
+++ src/reddit.ts
@@ -1,7 +1,23 @@
 import snoowrap = require('snoowrap');
 
-const r = new snoowrap({
-    userAgent: "web:didactic-spork:v1.0.0"
-});
+function fetchAnonymousToken() {
+    const form = new FormData();
+    form.set('grant_type', 'https://oauth.reddit.com/grants/installed_client');
+    form.set('device_id', 'DO_NOT_TRACK_THIS_DEVICE');
+    return fetch('https://www.reddit.com/api/v1/access_token', {
+        method: 'post',
+        body: form,
+        headers: { authorization: `Basic ${btoa('bNMBK9jcHAIISw:')}` },
+        credentials: 'omit',
+    }).then(response => response.text())
+        .then(JSON.parse)
+        .then(tokenInfo => tokenInfo.access_token)
+        .then(anonymousToken => {
+            const anonymousSnoowrap = new snoowrap({ userAgent: "web:didactic-spork:v1.0.0 (by /u/purisame)", accessToken: anonymousToken.toString() });
+            anonymousSnoowrap.config({ proxies: false, requestDelay: 1000 });
+            return anonymousSnoowrap;
+        });
+}
 
+const r = fetchAnonymousToken().catch(console.error);
 export { r };
