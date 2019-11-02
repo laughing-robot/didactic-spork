7ec7ba4250a095c04b9ae32c0b7ba6abc2e17221
Sameer Puri <sameer@users.noreply.github.com>
2019-11-02 02:21:48 -0500

    Implement basic demo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-alert('hello fam!');
+// https://stackoverflow.com/questions/1484506/random-color-generator
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+setInterval(() => {
+    document.getElementsByTagName('a-box')[0].setAttribute('color', getRandomColor());
+}, 1000);
